c2b0e9a6ccc5b92ecc1960e10807d5b1f3da3122
BisanGhoul <bbisann@gmail.com>
2023-03-10 22:12:08 +0200

    unsbcripe from observable after component is destroyed

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+
   user: {id: number, name: string};
+  paramsSubcription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,7 +21,7 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     //params is an observable
-    this.route.params.subscribe
+    this.paramsSubcription = this.route.params.subscribe
     (
       //it will be fired whenever new data is sent through observable
       //so, when the parameter change
@@ -28,4 +31,9 @@ export class UserComponent implements OnInit {
       }
     )
     }
+
+    //angular does this for us, but we should do it if we add our own observable
+    ngOnDestroy(): void {
+      this.paramsSubcription.unsubscribe();
+    } 
 }
