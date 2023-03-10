f216c6cc9427845cd4b538094a894626f4e3b692
BisanGhoul <bbisann@gmail.com>
2023-03-10 22:53:36 +0200

    fetch query params

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +13,18 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //if our query params change from the page we are currently on
+    //this wonr work because it is not reacive
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+  //alternative
+    this.route.params.subscribe();
+    this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
