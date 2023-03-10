5b6445ce7fc27b8882f6747bf73b1e089a7e4fcc
BisanGhoul <bbisann@gmail.com>
2023-03-10 21:04:03 +0200

    add button navigate programitically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  //inject router
+  constructor(private router: Router ) {}
 
   ngOnInit() {
+  }
+
+  onLoadServers (){
+    //complex calculation
+    this.router.navigate(['/servers']);//absolute path
   }
 
 }
