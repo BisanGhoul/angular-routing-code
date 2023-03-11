c4758c0489638aa33381e643c13081a2a4a5c86e
BisanGhoul <bbisann@gmail.com>
2023-03-11 16:08:12 +0200

    add edit server button and logic

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,7 +12,8 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     //convert id to a number
@@ -20,6 +21,12 @@ export class ServerComponent implements OnInit {
     this.route.params.subscribe(
       (params: Params) => {this.server=this.serversService.getServer(+params['id'])
   });
+  }
+
+  onEdit(){
+    // this.router.navigate(['/servers',+this.server.id,'edit']);
+    //or
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
